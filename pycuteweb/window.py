32daea73d489c8dcae05dc22e9e1aa027d7f1278
Matteo Meneghetti <matteo@meneghetti.dev>
2020-11-27 01:25:47 +0100

    External webpages are opened in the browser, solved size issue

--- pycuteweb/window.py
+++ pycuteweb/window.py
@@ -1,6 +1,7 @@
 from PySide2.QtCore import QUrl
-from PySide2.QtGui import QIcon
-from PySide2.QtWebEngineWidgets import QWebEngineView
+from PySide2.QtGui import QIcon, QDesktopServices
+from PySide2.QtWidgets import QDesktopWidget
+from PySide2.QtWebEngineWidgets import QWebEngineView, QWebEnginePage
 
 
 class Window:
@@ -8,12 +9,13 @@ class Window:
     def __init__(self, url, title, icon=None):
 
         self.__zoom = 1.0
-        self.__url = url
+        self.__start_url = url
         self.qtView = QWebEngineView()
 
         self.qtView.setWindowTitle(title)
         if icon is not None:
             self.qtView.setWindowIcon(QIcon(icon))
+        self.qtView.resize(QDesktopWidget().availableGeometry(self.qtView).size() * 0.75);
 
     def resize(self, width, height):
         self.qtView.resize(width, height)
@@ -29,5 +31,19 @@ class Window:
         self.qtView.setZoomFactor(self.zoom)
 
     def start(self):
-        self.qtView.setUrl(QUrl(self.__url))
+        self.qtView.setPage(WebPage(self.__start_url, parent=self.qtView))
         self.qtView.show()
+
+class WebPage(QWebEnginePage):
+
+    def __init__(self, url, parent=None):
+        super(WebPage, self).__init__(parent)
+        self.__url = url
+        self.load(QUrl(self.__url))
+    
+    def acceptNavigationRequest(self, qurl, navtype, mainframe):
+        if self.__url in qurl.toString():  # open in QWebEngineView
+            return True
+        else:
+            QDesktopServices.openUrl(qurl)
+            return False

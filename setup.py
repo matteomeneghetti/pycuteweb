2bae7c74362204a140004433ad22f9604e8589fa
Matteo Meneghetti <matteo@meneghetti.dev>
2020-11-29 11:23:49 +0100

    Decluttering

--- setup.py
+++ setup.py
@@ -18,8 +18,7 @@ setuptools.setup(
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3'
     ],
     install_requires=["pyside2"],
     python_requires='>=3.6'
